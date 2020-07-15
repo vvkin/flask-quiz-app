4a544b47cc3a9d3b292793c53e65adbf56233771
Vadym Kinchur <vkincur@gmail.com>
2020-07-15 03:45:14 +0300

    added jQuery passwords validation

--- flask-quiz-app/auth/static/js/script.js
+++ flask-quiz-app/auth/static/js/script.js
@@ -1,12 +1,12 @@
-function checkPasswords(){
-    firstPassword = getElementsByName('u_passwd')[0];
-    secondPassword = getElementsByName('us_passwd')[0];
-    if (firstPassword !== secondPassword){
-        getElementById('submit_button').enabled = false;
-        getElementById('flash_message').innerHTML = 'Passwords mismatch!'
-    }
-    else{
-        getElementById('submit_button').enabled = true;
-    }
-}
-
+$(document).ready(function(){
+    $('#f_passwd, #s_passwd').on('keyup', function(){
+        $('.error').hide();
+        if ($('#f_passwd').val() !== $('#s_passwd').val()){
+            $('#s_passwd').after('<span class="error">Passwords does not match!</span>');
+            $('#submit').attr('disabled', 'disabled');
+        }
+        else {
+            $('#submit').removeAttr('disabled');
+        }
+    });
+});
